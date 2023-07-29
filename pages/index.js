6e8506a0fb028e94ec64948a4e56911bf1f2a8ff
Isabsk <isabsk10@gmail.com>
2023-07-29 20:46:32 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -64,9 +64,9 @@ export default function Home({ posts, authorDetails }) {
             Latest Posts{' '}
           </h1>
           <div className="md:w-30 mb-4 mt-1 h-1 w-20 bg-gradient-to-r from-blue-500 to-cyan-400 md:h-2"></div>
-          <section class="body-font overflow-hidden text-gray-600">
-            <div class="container mx-auto px-5 py-24">
-              <ul class="-m-12 flex flex-wrap">
+          <section className="body-font overflow-hidden text-gray-600">
+            <div className="container mx-auto px-5 py-24">
+              <ul className="-m-12 flex flex-wrap">
                 {' '}
                 {!posts.length && 'No posts found.'}{' '}
                 {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
@@ -83,7 +83,7 @@ export default function Home({ posts, authorDetails }) {
                   return (
                     <li
                       key={slug}
-                      class="flex flex-col items-start p-12 md:w-1/2"
+                      className="flex flex-col items-start p-12 md:w-1/2"
                     >
                       <div className="relative mb-5 w-full pt-[60%]">
                         <Image
@@ -100,58 +100,58 @@ export default function Home({ posts, authorDetails }) {
                         ))}{' '}
                       </div>
                       <Link href={`/blog/${slug}`}>
-                        <h2 class="title-font mb-4 mt-4 text-2xl font-extrabold text-gray-900 sm:text-3xl">
+                        <h2 className="title-font mb-4 mt-4 text-2xl font-extrabold text-gray-900 sm:text-3xl">
                           {title}
                         </h2>
                       </Link>
-                      <p class="mb-8 font-light leading-relaxed">{summary}</p>
-                      <div class="mb-4 mt-auto flex w-full flex-wrap items-center border-b-2 border-gray-100 pb-4">
+                      <p className="mb-8 font-light leading-relaxed">{summary}</p>
+                      <div className="mb-4 mt-auto flex w-full flex-wrap items-center border-b-2 border-gray-100 pb-4">
                         <Link href={`/blog/${slug}`}>
-                          <a class="inline-flex items-center text-cyan-500">
+                          <a className="inline-flex items-center text-cyan-500">
                             Learn More
                             <svg
-                              class="ml-2 h-4 w-4"
+                              className="ml-2 h-4 w-4"
                               viewBox="0 0 24 24"
                               stroke="currentColor"
-                              stroke-width="2"
+                              strokeWidth="2"
                               fill="none"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
                             >
                               <path d="M5 12h14"></path>
                               <path d="M12 5l7 7-7 7"></path>
                             </svg>
                           </a>
                         </Link>
-                        <span class="ml-auto mr-3 inline-flex items-center border-r-2 border-gray-200 py-1 pr-3 text-[12px] leading-none text-gray-400">
+                        <span className="ml-auto mr-3 inline-flex items-center border-r-2 border-gray-200 py-1 pr-3 text-[12px] leading-none text-gray-400">
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="16"
                             height="16"
                             fill="currentColor"
-                            class="bi bi-calendar-event-fill"
+                            className="bi bi-calendar-event-fill"
                             viewBox="0 0 16 16"
                           >
                             <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z" />
                           </svg>
                         </span>
-                        <span class="inline-flex items-center text-[12px] leading-none text-gray-400">
+                        <span className="inline-flex items-center text-[12px] leading-none text-gray-400">
                           <time dateTime={date}> {formatDate(date)} </time>
                         </span>
                       </div>
-                      <a class="inline-flex items-center">
+                      <a className="inline-flex items-center">
                         <Image
                           alt="blog"
                           src={avatar}
                           height="45"
                           width="45"
-                          class="h-12 w-12 flex-shrink-0 rounded-full object-cover object-center"
+                          className="h-12 w-12 flex-shrink-0 rounded-full object-cover object-center"
                         />
-                        <span class="flex flex-grow flex-col pl-4">
-                          <span class="title-font text-md font-medium text-gray-900">
+                        <span className="flex flex-grow flex-col pl-4">
+                          <span className="title-font text-md font-medium text-gray-900">
                             {author}
                           </span>
-                          <p class="text-sm text-gray-500">Developer</p>
+                          <p className="text-sm text-gray-500">Developer</p>
                         </span>
                       </a>
                     </li>
