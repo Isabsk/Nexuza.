e2b49c474378f42ae0a29cd98017ba7e852b37e7
Isab Sk <isabsk10@gmail.com>
2024-03-17 09:53:16 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -109,10 +109,10 @@ export default function Home({ posts, authauthorDetailsorDetails }) {
                         {summary}
                       </p>
                       <div className="mb-4 mt-auto flex w-full flex-wrap items-center border-b-2 border-gray-100 pb-4">
-                        <Link href={`/blog/${slug}`}>
-                          <a className="inline-flex items-center text-cyan-500 dark:text-cyan-300">
+                        <Link href={`/blog/${slug}`} className="inline-flex items-center text-cyan-500 dark:text-cyan-300" legacyBehavior>
+                          
                             Learn More -->
-                          </a>
+                          
                         </Link>
                         <span className="ml-auto mr-3 inline-flex items-center border-r-2 border-gray-200 py-1 pr-3 text-[12px] leading-none text-gray-600 dark:text-gray-200">
                           <svg
