72b96c64aadab62784d123243df1cc9399cbc6bf
Isab Sk <isabsk10@gmail.com>
2024-03-17 01:26:26 +0000

    update

--- components/Tag.js
+++ components/Tag.js
@@ -2,15 +2,16 @@ import Link from 'next/link'
 import kebabCase from '@/lib/utils/kebabCase'
 
 const Tag = ({ text }) => {
-  return (
-    <>
-      <Link href={`/tags/${kebabCase(text)}`}>
-        <a className="mr-2 mt-2 inline-block rounded bg-gray-100 bg-gradient-to-br from-blue-500 to-cyan-400 px-3 py-1.5 text-xs font-extrabold uppercase tracking-widest text-white">
-          {text.split(' ').join('-')}
-        </a>
-      </Link>
-    </>
-  )
+  return <>
+    <Link
+      href={`/tags/${kebabCase(text)}`}
+      className="mr-2 mt-2 inline-block rounded bg-gray-100 bg-gradient-to-br from-blue-500 to-cyan-400 px-3 py-1.5 text-xs font-extrabold uppercase tracking-widest text-white"
+      legacyBehavior>
+
+      {text.split(' ').join('-')}
+
+    </Link>
+  </>;
 }
 
 export default Tag
--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export async function getStaticProps() {
   }
 }
 
-export default function Home({ posts, authorDetails }) {
+export default function Home({ posts, authauthorDetailsorDetails }) {
   return (
     <>
       <PageSEO
