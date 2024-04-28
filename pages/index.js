dc27c9ad65f54932f15c69ede491ca40b956ff5f
Isabsk <isabsk10@gmail.com>
2024-04-28 18:09:21 +0530

    newsletter added

--- pages/index.js
+++ pages/index.js
@@ -101,6 +101,9 @@ export default function Home({ posts, authauthorDetailsorDetails }) {
         </div>
 
       </div>
+      <div>
+      <iframe src="https://embeds.beehiiv.com/f20489b7-f7a3-4510-8b35-20d76fdfd67c" data-test-id="beehiiv-embed" width="100%" height="320" frameborder="0" scrolling="no" style="border-radius: 4px; border: 2px solid #e5e7eb; margin: 0; background-color: transparent;"></iframe>
+      </div>
     </div>
   
       <div className="relative" >
