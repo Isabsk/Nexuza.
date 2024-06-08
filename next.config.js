882f422995a1c87627ae59e4e4b63f514445773f
Isabsk <isabsk10@gmail.com>
2024-06-08 14:29:06 +0530

    all changes done

--- next.config.js
+++ next.config.js
@@ -4,15 +4,16 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
-  style-src 'self' 'unsafe-inline';
-  img-src * blob: data:;
-  media-src 'none';
-  connect-src *;
-  font-src 'self';
-  frame-src giscus.app;
-
+default-src * data: mediastream: blob: filesystem: about: ws: wss: 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline'; 
+script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; 
+script-src-elem * data: blob: 'unsafe-inline' 'unsafe-eval';
+connect-src * data: blob: 'unsafe-inline'; 
+img-src * data: blob: 'unsafe-inline'; 
+media-src * data: blob: 'unsafe-inline'; 
+frame-src * data: blob: ; 
+style-src * data: blob: 'unsafe-inline';
+font-src * data: blob: 'unsafe-inline';
+frame-ancestors * data: blob: 'unsafe-inline';
 `
 
 const securityHeaders = [
