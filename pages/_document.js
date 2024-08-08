b728c48558cb901d92bc8ff0807088d749bca342
Isabsk <isabsk10@gmail.com>
2024-08-08 13:48:47 +0530

    all changes done

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,9 @@ class MyDocument extends Document {
             httpEquiv="Content-Security-Policy"
             content="default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com giscus.app; connect-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline';"
           />
-          <meta 
+          <meta
             name="google-adsense-account"
-            content="ca-pub-3585939239731411" 
+            content="ca-pub-3585939239731411"
           />
           <meta
             httpEquiv="Content-Security-Policy"
