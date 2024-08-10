dc2cb85f3e29e0edf2334ded8a379684403ec681
Isabsk <isabsk10@gmail.com>
2024-08-10 13:23:37 +0530

    ga4 tag added

--- next.config.js
+++ next.config.js
@@ -2,69 +2,45 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-// You might need to insert additional domains in script-src if you are using external services
+// Content Security Policy configuration
 const ContentSecurityPolicy = `
   default-src 'self' data: mediastream: blob: filesystem: about: ws: wss: 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline'; 
   script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.clarity.ms https://plausible.io giscus.app; 
-  script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' https://www.clarity.ms https://plausible.io; 
-  connect-src 'self' data: blob: 'unsafe-inline'; 
-  img-src 'self' data: blob: 'unsafe-inline' https://www.google.co.in; 
+  script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.clarity.ms https://plausible.io giscus.app; 
+  connect-src 'self' https://www.clarity.ms https://plausible.io https://www.googletagmanager.com data: blob: 'unsafe-inline'; 
+  img-src 'self' data: blob: https://www.google.co.in; 
   media-src 'self' data: blob: 'unsafe-inline'; 
-  frame-src 'self' data: blob: https://embeds.beehiiv.com https://td.doubleclick.net; 
+  frame-src 'self' https://embeds.beehiiv.com https://td.doubleclick.net giscus.app; 
   style-src 'self' data: blob: 'unsafe-inline' https://fonts.googleapis.com; 
-  font-src 'self' data: blob: 'unsafe-inline'; 
+  font-src 'self' https://fonts.gstatic.com data: blob: 'unsafe-inline'; 
   frame-ancestors 'self' data: blob: 'unsafe-inline';
 `
 
-module.exports = {
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: ContentSecurityPolicy.replace(/\n/g, ''),
-          },
-        ],
-      },
-    ]
-  },
-}
-
 const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
   {
     key: 'Content-Security-Policy',
     value: ContentSecurityPolicy.replace(/\n/g, ''),
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
   {
     key: 'Referrer-Policy',
     value: 'strict-origin-when-cross-origin',
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
     key: 'X-Frame-Options',
     value: 'DENY',
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
   {
     key: 'X-Content-Type-Options',
     value: 'nosniff',
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
   {
     key: 'X-DNS-Prefetch-Control',
     value: 'on',
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
   {
     key: 'Strict-Transport-Security',
     value: 'max-age=31536000; includeSubDomains',
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
   {
     key: 'Permissions-Policy',
     value: 'camera=(), microphone=(), geolocation=()',
@@ -74,13 +50,6 @@ const securityHeaders = [
 module.exports = withBundleAnalyzer({
   images: {
     domains: ['res.cloudinary.com'],
-  },
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'"],
-      frameSrc: ['https://allowed-iframe-host.com'],
-      // ...
-    },
   },
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
