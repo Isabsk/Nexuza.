d21557b491f44484ab588746a895075bbbfeda80
Isabsk <isabsk10@gmail.com>
2024-10-01 12:54:17 +0530

    removed ga

--- pages/_app.js
+++ pages/_app.js
@@ -38,6 +38,16 @@ export default function App({ Component, pageProps }) {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
+      <GoogleAnalytics gaId="G-THFCERS795" />
+      <Script id="clarity-script" strategy="afterInteractive">
+        {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "mmzmim8tix");
+          `}
+      </Script>
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
