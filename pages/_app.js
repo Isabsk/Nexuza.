3ca237dea794ee0645089c140bed45837a158c99
Isabsk <isabsk10@gmail.com>
2024-10-14 20:28:29 +0530

    plausible tag added

--- pages/_app.js
+++ pages/_app.js
@@ -36,18 +36,14 @@ export default function App({ Component, pageProps }) {
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <script
+          defer
+          data-domain="nexuza.com"
+          src="https://plausible.io/js/script.js"
+        ></script>
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <GoogleAnalytics gaId="G-THFCERS795" />
-      <Script id="clarity-script" strategy="afterInteractive">
-        {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "mmzmim8tix");
-          `}
-      </Script>
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
